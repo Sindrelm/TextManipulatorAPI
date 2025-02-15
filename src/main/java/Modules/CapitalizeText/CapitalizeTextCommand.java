17fbee59e535b2875b77a3ae177c99796d69f2e7
Sindre <Sindrelmjoes@gmail.com>
2025-02-15 18:08:51 +0100

    Created CapitalizeWordCommand class

--- src/main/java/Modules/CapitalizeText/CapitalizeTextCommand.java
+++ src/main/java/Modules/CapitalizeText/CapitalizeTextCommand.java
@@ -6,12 +6,16 @@ public class CapitalizeTextCommand implements TextCommand {
   public CapitalizeTextCommand() {
   }
 
+  public String capitalizeFirstLetter(String text) {
+    return Character.toUpperCase(text.charAt(0)) + text.substring(1);
+  }
+
   public String execute(String text) {
     StringBuilder result = new StringBuilder();
     String[] splitText = text.split("\\. ");
 
     for (String string : splitText) {
-      result.append(". ").append(Character.toUpperCase(string.charAt(0))).append(string.substring(1));
+      result.append(". ").append(capitalizeFirstLetter(string));
     }
     return result.substring(2);
   }
---
+++ src/main/java/Modules/CapitalizeText/CapitalizeWordsCommand.java
@@ -0,0 +1,18 @@
+package Modules.CapitalizeText;
+
+public class CapitalizeWordsCommand extends CapitalizeTextCommand{
+
+  public CapitalizeWordsCommand() {
+  }
+
+  @Override
+  public String execute(String text) {
+    StringBuilder result = new StringBuilder();
+    String[] splitText = text.split(" ");
+
+    for (String string : splitText) {
+      result.append(" ").append(super.capitalizeFirstLetter(string));
+    }
+    return result.substring(1);
+  }
+}
