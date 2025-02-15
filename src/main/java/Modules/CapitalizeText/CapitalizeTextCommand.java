296c142b8adde251e575092e9912b9adaf2e832b
Sindre <Sindrelmjoes@gmail.com>
2025-02-15 17:44:29 +0100

    Created CapitalizeTextCommand class

---
+++ src/main/java/Modules/CapitalizeText/CapitalizeTextCommand.java
@@ -0,0 +1,18 @@
+package Modules.CapitalizeText;
+import Modules.TextCommand;
+
+public class CapitalizeTextCommand implements TextCommand {
+
+  public CapitalizeTextCommand() {
+  }
+
+  public String execute(String text) {
+    StringBuilder result = new StringBuilder();
+    String[] splitText = text.split(". ");
+
+    for (String string : splitText) {
+      result.append(". ").append(Character.toUpperCase(string.charAt(0))).append(string.substring(1));
+    }
+    return result.toString();
+  }
+}
