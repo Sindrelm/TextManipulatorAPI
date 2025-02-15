a6a85e2bf0c5b0a17d8c17bcb12cbe91eb065966
Sindre <Sindrelmjoes@gmail.com>
2025-02-15 16:28:58 +0100

    Created test for wrapTextCommand

---
+++ src/test/java/TestWrapTextCommand.java
@@ -0,0 +1,34 @@
+import Modules.ReplaceText.ReplaceTextCommand;
+import Modules.WrapText.WrapTextCommand;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class TestWrapTextCommand {
+
+  @Test
+  public void testEmptyOpeningInConstructor() {
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+      new WrapTextCommand("", "ending");
+    });
+    assertEquals("String cannot be blank", exception.getMessage());
+  }
+
+  @Test
+  public void testGetOpening() {
+    WrapTextCommand command = new WrapTextCommand("opening", "ending");
+    assertEquals("opening", command.getOpening());
+  }
+
+  @Test
+  public void testGetEnding() {
+    WrapTextCommand command = new WrapTextCommand("opening", "ending");
+    assertEquals("ending", command.getEnding());
+  }
+
+  @Test
+  public void testExecute() {
+    WrapTextCommand command = new WrapTextCommand("<", ">");
+    assertEquals("<text>", command.execute("text"));
+  }
+}
