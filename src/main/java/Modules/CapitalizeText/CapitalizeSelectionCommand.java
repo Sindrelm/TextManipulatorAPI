df4276b87eac861e3f58fdd5617d44a2f6527e02
Sindre <Sindrelmjoes@gmail.com>
2025-02-15 18:22:31 +0100

    finished CapitalizeSelectionCommand

--- src/main/java/Modules/CapitalizeText/CapitalizeSelectionCommand.java
+++ src/main/java/Modules/CapitalizeText/CapitalizeSelectionCommand.java
@@ -1,10 +1,23 @@
 package Modules.CapitalizeText;
 
+import Modules.ReplaceText.ReplaceTextCommand;
+
 public class CapitalizeSelectionCommand extends CapitalizeTextCommand{
 
   private String selection;
 
   public CapitalizeSelectionCommand(String selection) {
+    verifyText(selection);
     this.selection = selection;
+  }
+
+  public String getSelection() {
+    return selection;
+  }
+
+  @Override
+  public String execute(String text) {
+    ReplaceTextCommand replaceCommand = new ReplaceTextCommand(selection, super.execute(selection));
+    return replaceCommand.execute(text);
   }
 }
